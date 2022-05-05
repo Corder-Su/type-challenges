ec101e81afa7f7e2253721d8d5c5c23f34da2410
Zekun Su <18702139357@163.com>
2022-05-05 18:38:05 +0800

    004easy-pick
    笔记

--- solutions/004easy-pick/template.ts
+++ solutions/004easy-pick/template.ts
@@ -2,6 +2,14 @@ type SuPick<T, K extends keyof T> = {
     [P in K] : T[P] // T中可能不存在P, 需要在使用前加判断
 }
 
+
+/**
+ * 知识点：
+1. [P in K] // K = "title" | "name" 联合类型的遍历；
+2. T[P] // T = { title : string }  取得该索引对应的类型；
+3. extends  //  泛型约束
+4. keyof  // 索引作为一个联合类型返回
+ */
 /**
  * interface Todo {
       title: string
